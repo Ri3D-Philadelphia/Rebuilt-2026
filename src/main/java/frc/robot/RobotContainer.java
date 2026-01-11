dc805e33f8bc08c566570821bb187e428d43afee
Ria Saheta <100453801+peanutz28@users.noreply.github.com>
2026-01-11 14:21:27 -0500

    button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,10 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.ControllerConstants;
 import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.fuelIntakePivot.FuelIntakePivot;
@@ -16,7 +20,30 @@ public class RobotContainer {
     private FuelIntakeRoller fuelIntakeRoller = new FuelIntakeRoller();
     private Indexer indexer = new Indexer();
     private ShooterHood shooterHood = new ShooterHood();
-    private ShooterFlywheel shooterFlywheel = new ShooterFlywheel();
+    private final ShooterFlywheel m_shooterFlywheel = new ShooterFlywheel();
     private Climber climber = new Climber();
-    
-}
+
+     private final CommandXboxController m_driverController =
+      new CommandXboxController(ControllerConstants.kDriverControllerPort);
+
+     public RobotContainer() {
+
+        configureBindings();
+
+        m_shooterFlywheel.setDefaultCommand(m_shooterFlywheel.set(0));
+
+     }
+
+    private void configureBindings() {
+        // Configure your button bindings here
+         // Schedule `setVelocity` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        m_driverController.a().whileTrue(m_shooterFlywheel.setSpeed(RotationsPerSecond.of(60)));
+        m_driverController.b().whileTrue(m_shooterFlywheel.setSpeed(RotationsPerSecond.of(300)));
+        // Schedule `set` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        m_driverController.x().whileTrue(m_shooterFlywheel.set(0.3));
+        m_driverController.y().whileTrue(m_shooterFlywheel.set(-0.3));
+
+
+    }}
