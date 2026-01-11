a6008ad86fe688685779d15a5dba397433cebe9d
Ria Saheta <100453801+peanutz28@users.noreply.github.com>
2026-01-11 16:10:36 -0500

    pigeon additions

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,10 @@ package frc.robot;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.drivetrain.Pigeon2Gyro;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -27,7 +29,7 @@ public class Robot extends TimedRobot {
 
   /**
    * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * initialization code.  
    */
   public Robot() {
     m_robotContainer = new RobotContainer();
@@ -36,6 +38,8 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
     DataLogManager.start();
+
+    // Shuffleboard.getTab("Pigeon").add(gyro);
   }
 
   /**
@@ -46,7 +50,20 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    try {
+    Pigeon2Gyro pigeon = m_robotContainer.getPigeon();
+    if (pigeon != null) {
+      double yawDeg = pigeon.getHeading().getDegrees();
+      SmartDashboard.putNumber("PigeonYawDeg", yawDeg);
+    } else {
+      SmartDashboard.putString("PigeonYawDeg", "pigeon null");
+    }
+  } catch (Throwable t) {
+    SmartDashboard.putString("PigeonYawDeg", "err: " + t.getMessage());
+    t.printStackTrace();
+  }
+  }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,14 @@
 package frc.robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.drivetrain.Pigeon2Gyro;
+
 
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
@@ -14,6 +19,8 @@ import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.shooterFlywheel.ShooterFlywheel;
 import frc.robot.subsystems.shooterHood.ShooterHood;
 
+
+
 @Logged
 public class RobotContainer {
     private SwerveDrive swerveDrive = new SwerveDrive();
@@ -23,6 +30,12 @@ public class RobotContainer {
     private ShooterHood shooterHood = new ShooterHood();
     private ShooterFlywheel shooterFlywheel = new ShooterFlywheel();
     private Climber climber = new Climber();
+    private final Pigeon2Gyro pigeon = new Pigeon2Gyro(30); // CAN ID 30
+
+    
+        public Pigeon2Gyro getPigeon() {
+            return this.pigeon;
+        }
 
     // gamepads
     private CommandXboxController driver = new CommandXboxController(0);
@@ -39,12 +52,28 @@ public class RobotContainer {
             * SwerveDriveConstants.kJoyAngleSpeedFactor;
 
     public RobotContainer() {
+
+        // call in a periodic or button action:
         
         swerveDrive.setDefaultCommand(swerveDrive.driveFieldCentric(
             driverForward.getAsDouble(), 
             driverStrafe.getAsDouble(), 
             driverTurn.getAsDouble()
         ));
+
+                // Using CommandXboxController driver (you have this)
+        driver.y().onTrue(new InstantCommand(() -> {
+            pigeon.zeroHeading();
+            System.out.println("[PIGEON] zeroed");
+        }));
+
+
+
+        // new RunCommand(() -> {
+        //     double yawDeg = pigeon.getHeading().getDegrees();
+        //     SmartDashboard.putNumber("PigeonYawDeg", yawDeg);      // Shuffleboard / SmartDashboard
+        //     System.out.println("[PIGEON] yaw deg = " + yawDeg);   // console log (driver station)
+        // }).schedule();
     }
     
 }
