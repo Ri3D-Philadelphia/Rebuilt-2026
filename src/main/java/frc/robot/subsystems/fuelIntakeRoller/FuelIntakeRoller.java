fd0f011602ddc3a52fa4432fce239ec5aab60f47
peanutz28 <100453801+peanutz28@users.noreply.github.com>
2026-01-11 21:30:24 -0500

    impl fuel intake roller (#3)
    Co-authored-by: Stephen Cai <KK201431873@gmail.com>

--- src/main/java/frc/robot/subsystems/fuelIntakeRoller/FuelIntakeRoller.java
+++ src/main/java/frc/robot/subsystems/fuelIntakeRoller/FuelIntakeRoller.java
@@ -3,7 +3,10 @@
  */
 package frc.robot.subsystems.fuelIntakeRoller;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Logged
@@ -15,6 +18,27 @@ public class FuelIntakeRoller extends SubsystemBase {
 
     public FuelIntakeRoller() {
         this.io = new FuelIntakeRollerIOSpark();
+    }
+
+    /**
+     * Returns a Command to spin the intake inwards.
+     */
+    public Command rollIn() {
+        return run(() -> io.setRollerVoltage(FuelIntakeRollerConstants.kRollerInVoltage));
+    }
+
+    /**
+     * Returns a Command to spin the intake outwards.
+     */
+    public Command rollOut() {
+        return run(() -> io.setRollerVoltage(FuelIntakeRollerConstants.kRollerOutVoltage));
+    }
+
+    /**
+     * Returns a Command to stop the intake roller.
+     */
+    public Command stopIntake() {
+        return run(() -> io.setRollerVoltage(Volts.of(0)));
     }
     
 }
