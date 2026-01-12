845ed0bfec42d7004768a397ffd56d80e9310530
Stephen Cai <KK201431873@gmail.com>
2026-01-11 21:39:16 -0500

    clean up robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,27 +4,15 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants.ControllerConstants;
-import static edu.wpi.first.units.Units.Volts;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.climber.Climb;
 import frc.robot.subsystems.drivetrain.Pigeon2Gyro;
 
 
 import java.util.function.DoubleSupplier;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.drivetrain.SwerveDriveConstants;
 import frc.robot.subsystems.fuelIntakePivot.FuelIntakePivot;
@@ -43,7 +31,7 @@ public class RobotContainer {
     private Indexer indexer = new Indexer();
     private ShooterHood shooterHood = new ShooterHood();
     private final ShooterFlywheel m_shooterFlywheel = new ShooterFlywheel();
-    private Climber climber = new Climber();
+    private Climb climb = new Climb();
 
   
     private final Pigeon2Gyro pigeon = new Pigeon2Gyro(30); // CAN ID 30
