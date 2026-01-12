3d61b63fbde79456a6462112ed90e70327ff7351
peanutz28 <100453801+peanutz28@users.noreply.github.com>
2026-01-11 21:29:34 -0500

    Shooter subsystem (#2)
    * set up flywheel code, not hood yet. needs correct canIDs
    
    * button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.ControllerConstants;
 import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.drivetrain.Pigeon2Gyro;
@@ -33,12 +38,14 @@ import frc.robot.subsystems.shooterHood.ShooterHood;
 @Logged
 public class RobotContainer {
     private SwerveDrive swerveDrive = new SwerveDrive();
-    // private FuelIntakePivot fuelIntakePivot = new FuelIntakePivot();
-    // private FuelIntakeRoller fuelIntakeRoller = new FuelIntakeRoller();
-    // private Indexer indexer = new Indexer();
-    // private ShooterHood shooterHood = new ShooterHood();
-    // private ShooterFlywheel shooterFlywheel = new ShooterFlywheel();
-    // private Climber climber = new Climber();
+    private FuelIntakePivot fuelIntakePivot = new FuelIntakePivot();
+    private FuelIntakeRoller fuelIntakeRoller = new FuelIntakeRoller();
+    private Indexer indexer = new Indexer();
+    private ShooterHood shooterHood = new ShooterHood();
+    private final ShooterFlywheel m_shooterFlywheel = new ShooterFlywheel();
+    private Climber climber = new Climber();
+
+  
     private final Pigeon2Gyro pigeon = new Pigeon2Gyro(30); // CAN ID 30
 
     
@@ -60,8 +67,26 @@ public class RobotContainer {
         () -> -MathUtil.applyDeadband(driver.getRightX(), SwerveDriveConstants.kAngleDeadband)
             * SwerveDriveConstants.kJoyAngleSpeedFactor;
 
-    public RobotContainer() {
+     public RobotContainer() {
 
+        configureBindings();
+
+        m_shooterFlywheel.setDefaultCommand(m_shooterFlywheel.set(0));
+
+     }
+
+    private void configureBindings() {
+        // Configure your button bindings here
+         // Schedule `setVelocity` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        driver.a().whileTrue(m_shooterFlywheel.setSpeed(RotationsPerSecond.of(60)));
+        driver.b().whileTrue(m_shooterFlywheel.setSpeed(RotationsPerSecond.of(300)));
+        // Schedule `set` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        driver.x().whileTrue(m_shooterFlywheel.set(0.3));
+        driver.y().whileTrue(m_shooterFlywheel.set(-0.3));
+      
+      
         // call in a periodic or button action:
 
         // new RunCommand(() -> {
@@ -112,6 +137,13 @@ public class RobotContainer {
         //     SmartDashboard.putNumber("PigeonYawDeg", yawDeg);      // Shuffleboard / SmartDashboard
         //     System.out.println("[PIGEON] yaw deg = " + yawDeg);   // console log (driver station)
         // }).schedule();
-    }
+   
+      
+    }      
+
+
+   
+
     
 }
+
--- src/main/java/frc/robot/subsystems/shooterHood/ShooterHood.java
+++ src/main/java/frc/robot/subsystems/shooterHood/ShooterHood.java
@@ -1,7 +1,35 @@
 package frc.robot.subsystems.shooterHood;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.PivotConfig;
+import yams.mechanisms.positional.Pivot;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class ShooterHood extends SubsystemBase {
+    
+
     
 }
